53b59b4fa47978ed0d36996f235b6d9f0fb5e5ae
gameglobaljam <ggj@ffb.local>
2019-01-26 19:02:46 -0300

    First Sprites

--- Assets/Scripts/AuntM.cs
+++ Assets/Scripts/AuntM.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class AuntM : MonoBehaviour
 {
 	private Rigidbody2D rgdbd;
+	private Animator    anmtr;
     // Start is called before the first frame update
     void Start()
-    {
+	{
 		rgdbd = GetComponent<Rigidbody2D> ();
+		anmtr = GetComponent<Animator> ();
     }
 
     // Update is called once per frame
     void Update()
 	{
 		if (Input.GetKey ("a") || Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.localScale = new Vector3 (-0.1f, 0.1f, 0.1f);
 			rgdbd.AddForce (Vector2.left * 3);
 		}
 		if (Input.GetKey ("d") || Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.localScale = new Vector3 (0.1f, 0.1f, 0.1f);
 			rgdbd.AddForce (Vector2.right * 3);
 		}
 		if (Input.GetKeyDown ("w") || Input.GetKeyDown (KeyCode.UpArrow)) {
 			rgdbd.AddForce (Vector2.up * 300);
 		}
+		anmtr.SetBool("idle",(rgdbd.velocity == Vector2.zero));
     }
 
 	void OnTriggerEnter(Collider other){
